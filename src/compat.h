d070c790c2552c8ea52fe3093d62ddc31fa7e557
Cory Fields <cory-nospam-@coryfields.com>
2015-02-17 16:15:01 -0500

    0.10 fix for crashes on OSX 10.6
    strnlen is available at build-time but not at runtime, causing a crash.
    
    0.11 drops support for 10.6, so this is not needed in master.

--- src/compat.h
+++ src/compat.h
@@ -82,9 +82,7 @@ typedef int32_t ssize_t;
 #endif
 #endif
 
-#if HAVE_DECL_STRNLEN == 0
-size_t strnlen( const char *start, size_t max_len);
-#endif // HAVE_DECL_STRNLEN
+size_t strnlen_int( const char *start, size_t max_len);
 
 bool static inline IsSelectableSocket(const SOCKET& s) {
 #ifdef WIN32
